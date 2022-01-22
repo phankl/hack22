36e12ae1b3db3a26b7770a51090140918d2652c7
tekeh <te276@cam.ac.uk>
2022-01-22 20:10:18 +0000

    Z is truly now a collection of nodes. Removed dead code. Contingency table logic all included now. Extensive tests still pending.

--- node_independence.py
+++ node_independence.py
@@ -5,6 +5,15 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 import statsmodels.api as sm
 
+## Constants
+
+MIN_DATASET_SIZE=1000
+P_VALUE_CRITERION = 0.05
+FILEPATH = "data/2005-tfldata-accidents.csv" # Note this is a local path - must change for server (tekeh)
+TEST_FEATURES = ['Accident Severity', 'Road Type', 'Time', 'Light Conditions (Banded)', 'Road Surface', 'Weather'] ## The subset we will build the model on
+
+# Functions
+
 def is_independent(dat, X, Y, Z=None):
     """
     Checks whether X|Z is independent of Y. Z is a set of covariates, whereas X, Y are single covariates
@@ -12,68 +21,56 @@ def is_independent(dat, X, Y, Z=None):
     X : column label
     Y : column label
     Z : column labels (1 for now, but will generalise)
-    Works for categorical data at the moment
+
+    Works for categorical data at the moment - continuous data is even easier
     """
-    ## do the linear regression : STATSMODELS
-    
     ## Construct contingency table and work out nominal dependence 
+    res_list = []
+    pvalues_list = []
     if Z is None:
         ## Testing direct dependence
-        tab = pd.crosstab(dat[X], dat[Y])
-        table = sm.stats.Table(tab)
+        #tab = pd.crosstab(dat[X], dat[Y])
+        table = sm.stats.Table.from_data( df[X+Y][dat[Z] == cat] ) 
         res = table.test_nominal_association()
         pval = res.pvalue
 
     else:
-        ## In this branch we deal with the set Z not empty. Easiest way is to take some const Z cut of the data
-        controlled_categories = dat[Z].unique()
-        pvalues_list = []
+        ## Here e deal with the set Z not empty. Take const Z cuts of the data and look for indep.
+
+        ## Create new column which is a tupled version of the control variables
+        
+        dat['Combined_Controls'] = list(zip(*[dat[x] for x in Z]))
+        controlled_categories = dat['Combined_Controls'].unique()
         for cat in controlled_categories:
-            print(cat)
-            xdat = dat[X][dat[Z] == cat]
-            ydat = dat[Y][dat[Z] == cat]
+            #print(cat)
             try:
-                tab = pd.crosstab(xdat, ydat)
-                table = sm.stats.Table(tab)
+                dat_subset = dat[X+Y][dat['Combined_Controls'] == cat]
+                if dat_subset.shape[0] < MIN_DATASET_SIZE:
+                    continue
+                table = sm.stats.Table.from_data( dat_subset ) 
                 res = table.test_nominal_association()
                 pvalues_list.append(res.pvalue)
+                res_list.append(res)
             except:
                 continue
-        print(pvalues_list)
-        pval = max(pvalues_list) ## crude, but should work
+        #print(pvalues_list)
+        pval = max(pvalues_list) ## crude. Prob of observing assuming null hyp (two variables are independent)
     
-    print(pval)
-    if pval < P_VALUE_CRITERION: ## 5% significance, say
-        return 1
+    #print(pval)
+    if pval < P_VALUE_CRITERION: ## 5% significance, say. Reject the null 
+        print(f"{X} | {Z} depends on {Y}")
+        return 1, res_list, pvalues_list
     else:
-        return 0
+        print(f"{X} | {Z} does not depend on {Y}")
+        return 0, res_list, pvalues_list
 
 
 if __name__ == "__main__":
-    P_VALUE_CRITERION = 0.05
-    FILEPATH = "data/2005-tfldata-accidents.csv" # Note this is a local path - must change for server (tekeh)
-    TEST_FEATURES = ['Accident Severity', 'Road Type', 'Time', 'Light Conditions (Banded)', 'Road Surface', 'Weather'] ## The subset we will build the model on
 
     dat = pd.read_csv(FILEPATH)[TEST_FEATURES] ## truncated set
-    X, Y, Z = ['Accident Severity'], ['Road Surface'], ['Weather']
-    #is_independent(dat, X[0], Y[0])
-    is_independent(dat, X[0], Y[0], Z[0])
+    Y = ['Road Surface']
+    X = ['Accident Severity']
+    Z = ['Weather']
+    bit, res_list, p_list = is_independent(dat, Y, X, Z)
 
-
-    #dat = dat[X+Z]
-    #dat[X] = pd.factorize(dat[X])
-    #dat[Z] = pd.factorize(dat[Z])
-
-    #reg_y, enc_y = pd.factorize(dat[X])
-    #reg_X1, enc_X1 = pd.factorize(dat[Y])
-    #reg_X2, enc_X2 = pd.factorize(dat[Z])
-
-
-
-    #reg_y = reg_y.reshape(-1,1)
-    #reg_X1 = reg_X1.reshape(-1,1)
-    #reg_X2 = reg_X2.reshape(-1,1)
-    #reg_X = np.hstack((reg_X1, reg_X2))
-    #reg_model = sm.OLS(reg_y, reg_X)
-    #results = reg_model.fit()
-    #print(results.summary())
+    ## Try 
