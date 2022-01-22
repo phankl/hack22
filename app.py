d88d0fae3558119e4c501d4ecfa41fc160fc1ad6
Rachel Halpern <rachelhalpern438@gmail.com>
2022-01-22 15:12:57 +0000

    table only getting 1 column made

--- app.py
+++ app.py
@@ -25,38 +25,47 @@ def upload_csv():
 	column_names = [col for col in df.columns]
 
 	#connect to database
-	#conn = sqlite3.connect('CSV_DATA.db')
-	#c = conn.cursor()
+	conn = sqlite3.connect('CSV_DATA.db')
+	c = conn.cursor()
 
 	#right now, only supports one table of data
 	#table vals is a string of the column names
-	table_vals = ""
-	for col in column_names:
+	table_vals = ''+column_names[0]
+	if col_types[column_names[0]] == 'float64':
+		table_vals += " FLOAT PRIMARY KEY,"
+	elif col_types[column_names[0]] == 'int64':
+		table_vals += " INT PRIMARY KEY,"
+	elif col_types[column_names[0]] == 'object':
+		table_vals += " VARCHAR(255) PRIMARY KEY,"
+
+	for col in column_names[1:]:
 		table_vals = table_vals+" "+col
 		if col_types[col] == 'float64':
-			table_vals += " real,"
+			table_vals += " FLOAT,"
 		elif col_types[col] == 'int64':
-			table_vals += " real,"
+			table_vals += " INT,"
 		elif col_types[col] == 'object':
-			table_vals += " text,"
-	table_vals = table_vals.rstrip(',')
-	table_vals = table_vals[1:]
+			table_vals += " VARCHAR(255),"
+	#table_vals = table_vals.rstrip(',')
+	#table_vals = table_vals[1:len(table_vals)-1]
+	table_vals = table_vals[:len(table_vals)-1]
 
 	#create table
-	#c.execute('CREATE TABLE if not exists T1 ('+table_vals+')')
+	c.execute('CREATE TABLE if not exists T1 ('+table_vals+')')
 
 	#insert csv data into the table
 	for i, j in df.iterrows():
-		names_string = '('
+		row_data = '('
 		for col in column_names:
-			names_string = names_string + "'" + str(j[col])+"',"
-		names_string = names_string.rstrip(',')
-		#if names_string != '':
-			#c.execute("INSERT INTO T1 VALUES "+names_string+")")
-			#conn.commit()
-	#return "csv data in db!"
-	d = {"table_vals":table_vals, "names_string":names_string, "column_names":column_names}
-	return d
+			row_data = row_data + "'" + str(j[col])+"',"
+		row_data = row_data.rstrip(',')
+		if row_data != '':
+			c.execute("INSERT INTO T1 VALUES "+row_data+")")
+			conn.commit()
+	return "csv data in db!"
+	#d = {"table_vals":table_vals, "row_data":row_data, "column_names":column_names}
+	#return d
+	#return " 'CREATE TABLE if not exists T1 ('"+table_vals+"')'"
 
 #direction to display the CSV data
 @app.route('/display_csv', methods=['POST', 'GET'])
