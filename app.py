905a69c0ebdba712290f50f6714fda8547a716b0
Rachel Halpern <rachelhalpern438@gmail.com>
2022-01-22 14:30:05 +0000

    upload and dataprocessing on right track

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, render_template, Response
+
+import sqlite3
+import pandas as pd
+
+app = Flask(__name__)
+
+# connect database with csv
+# put csv to database --> put csv into mysql database
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+#direction for uploading CSV file
+@app.route('/upload_csv', methods=['POST'])
+def upload_csv():
+	#extract file path entry and read contents into dataframe
+	file = request.files['file']
+	#if not file:
+		#return "No file"
+	df = pd.read_csv(file)
+
+	#get column types and column names
+	col_types = (df.dtypes).to_dict()
+	#check for unnamed columns?
+	column_names = [col for col in df.columns]
+
+	#connect to database
+	
+	#conn = sqlite3.connect('CSV_DATA.db')
+	#c = conn.cursor()
+
+	#right now, only supports one table of data
+	table_vals = ""
+	for col in column_names:
+		table_vals = table_vals+" "+col
+	table_vals = table_vals.rstrip(',')
+
+	#create table
+	#c.execute('CREATE TABLE if not exists T1 ('+table_vals+')')
+
+	#insert csv data into the table
+	for i, j in df.iterrows():
+		names_string = '('
+		for col in column_names:
+			names_string = names_string + "'" + str(j[col])+"',"
+		names_string = names_string.rstrip(',')
+	#	if names_string != '':
+	#		c.execute("INSERT INTO T1 VALUES "+names_string+")")
+	#conn.commit()
+	#return "csv data in db!"
+	d = {"table_vals":table_vals, "names_string":names_string, "column_names":column_names}
+	return d
+
+#direction to display the CSV data
+@app.route('/display_csv', methods=['POST', 'GET'])
+def display_csv():
+	#connect
+	cn = sqlite3.connect('CSV_DATA.db')
+	df = pd.read_sql_query("SELECT * FROM T1", cn)
+	return render_template("display_csv.html", data=df.to_html())
+
+if __name__ == "__main__":
+	app.run()
+
+
+
+
