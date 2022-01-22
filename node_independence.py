db6264930d5d8d7854a5fbee3f1a04ce01bdb8d5
tekeh <te276@cam.ac.uk>
2022-01-22 23:04:55 +0000

    Minor cosmetic changes

--- node_independence.py
+++ node_independence.py
@@ -10,12 +10,12 @@ import statsmodels.api as sm
 MIN_DATASET_SIZE=1000
 P_VALUE_CRITERION = 0.01
 FILEPATH = "data/2005-tfldata-accidents.csv" # Note this is a local path - must change for server (tekeh)
-TEST_FEATURES = ['Accident Severity', 'Road Type', 'Time', 'Light Conditions (Banded)', 'Road Surface', 'Weather'] ## The subset we will build the model on
+TEST_FEATURES_TFL = ['Accident Severity', 'Road Type', 'Time', 'Light Conditions (Banded)', 'Road Surface', 'Weather'] ## The subset we will build the model on
 DAYLIGHT_HORIZON = 1800
 
 # Functions
 
-def is_independent(dat, X, Y, Z=None):
+def is_independent(dat, X, Y, Z=None, categorical=True):
     """
     Checks whether X|Z is independent of Y. Z is a set of covariates, whereas X, Y are single covariates
     dat : pandas dataframe
@@ -31,7 +31,7 @@ def is_independent(dat, X, Y, Z=None):
     if Z is None:
         ## Testing direct dependence
         #tab = pd.crosstab(dat[X], dat[Y])
-        table = sm.stats.Table.from_data( df[X+Y][dat[Z] == cat] ) 
+        table = sm.stats.Table.from_data(dat[X+Y]) 
         res = table.test_nominal_association()
         pval = res.pvalue
 
@@ -64,29 +64,35 @@ def is_independent(dat, X, Y, Z=None):
         print(f"{X} | {Z} does not depend on {Y}")
         return 0, res_list, pvalues_list
 
-def time_preprocess(dat):
+def tfl_preprocess(dat):
     """
-    Helper function to postprocess tfl data time field
+    Helper function to preprocess TfL data fields
     """
+    ## Time field
     dat.Time = pd.to_numeric(dat.Time.str[1:]) # postprocess for tfl data
     x = dat.Time < DAYLIGHT_HORIZON
     dat.Time = x.map({True:1, False:2})
     return dat ## for success
-    
-    
+
+def mtg_preprocess(dat):
+    dat.prices = [x['usd'] for x in dat.prices]
+    return dat
 
 if __name__ == "__main__":
 
-    dat = pd.read_csv(FILEPATH)[TEST_FEATURES] ## truncated set
-    dat = time_preprocess(dat)
-    Y = ['Road Surface']
-    X = ['Accident Severity']
-    Z = ['Weather']
-    bit, res_list, p_list = is_independent(dat, X, Y, Z)
-
+    #Y = ['Road Surface']
+    #X = ['Accident Severity']
+    #Z = ['Weather']
+    #bit, res_list, p_list = is_independent(dat, X, Y, Z)
+    
+    ## Testset TFL
+    dat = pd.read_csv(FILEPATH)[TEST_FEATURES_TFL] ## truncated set
+    dat = tfl_preprocess(dat)
     ## Test: Do all possible combs (with |Z| = 1) to see the results and intuitively evaluate
     from itertools import product, combinations
-    for X, Y, Z in combinations(TEST_FEATURES, r=3):
+    for X, Y, Z in combinations(TEST_FEATURES_TFL, r=3):
         #print(X,Y,Z)
         bit, res_list, p_list = is_independent(dat, [X], [Y], [Z])
 
+    ## Testet MTG
+    ## Test
