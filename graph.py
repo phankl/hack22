d8a1afeb1832f8af7a0628f5e586e7385865cee3
Rachel Halpern <rachelhalpern438@gmail.com>
2022-01-23 00:22:02 +0000

    png file save iter

--- graph.py
+++ graph.py
@@ -52,7 +52,7 @@ def check_graph(n, b):
       g = nx.convert_matrix.from_numpy_matrix(a)
       nx.draw(g, arrows=True)
       #plt.show()
-      plt.savefig('graph.png')
+      plt.savefig('graph'+b+'.png')
       return True
  
 def classify_path_nodes(a, path):
@@ -200,7 +200,7 @@ def generate_dags(n, name):
   #print(n, end-start)
 
 
-#model_search(FILEPATH, TEST_FEATURES_TFL)
-models = model_search(FILEPATH, TEST_FEATURES_TFL)
-print(models)
+model_search(FILEPATH, TEST_FEATURES_TFL)
+#models = model_search(FILEPATH, TEST_FEATURES)
+#print(models)
 
